fd6924663d883e974ced4a3faba1d04fb135cc83
Barry Byford <barry_byford@yahoo.co.uk>
2019-04-22 20:46:09 +0100

    Bump to version 0.0.4

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.3',
+    version='0.0.4',
 
     description='Python library for support of 96board mezzanine micromez',
     long_description=long_description,
